fc3472e077137c68322b46ccc5b8a46a90e67b9b
BrandoLogo <117051256+BrandoLogo@users.noreply.github.com>
2023-11-17 13:15:26 -1000

    Add files via upload

--- bot.py
+++ bot.py
@@ -83,22 +83,22 @@ except:
 	with open("data.pickle", "wb") as f:
 		pickle.dump((words, labels, training, output), f)
 
-# model
-tensorflow.compat.v1.reset_default_graph()
-
-net = tflearn.input_data(shape =[None, len(training[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
-net = tflearn.regression(net)
-
-# training model
-# Improvement for accuracy: add more patterns, maybe get input from UH students themselves?
-model = tflearn.DNN(net)
-
 try:
 	model.load("model.tflearn")
 except:
+	# model
+	tensorflow.compat.v1.reset_default_graph()
+
+	net = tflearn.input_data(shape =[None, len(training[0])])
+	net = tflearn.fully_connected(net, 8)
+	net = tflearn.fully_connected(net, 8)
+	net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+	net = tflearn.regression(net)
+
+	# training model
+	# Improvement for accuracy: add more patterns, maybe get input from UH students themselves?
+	model = tflearn.DNN(net)
+
 	# Looking at the data
 	model.fit(training, output, n_epoch=500, batch_size=8, show_metric=True)
 	model.save("model.tflearn")
@@ -123,6 +123,7 @@ def chat():
 	while True:
 		inp = input("\nYou: ")
 		if inp.lower() == "bye":
+			print(f"\nGoodbye, I hope I answered all your issues!")
 			break
 
 		# Model Prediction might need some work -Brandon
@@ -134,8 +135,12 @@ def chat():
 		for tg in data["intents"]:
 			if tg['tag'] ==  tag:
 				responses = tg['responses']
+				if tag != "greeting":
+					article = tg['article']
 
-		print(f"\nbot: {random.choices(responses)}")
+		print(f"\nBot: {random.choices(responses)}")
+		if tag != "greeting":
+			print(f"\nFor more information check out the website: {article}")
 		
 
 chat()
